cfc87a5ffef321eb6e41232a2b1843d8050d9558
May <ben.may@mullenloweprofero.com>
2019-01-28 13:55:39 +0000

    Push

---
+++ ex3-3.rb
@@ -0,0 +1,2 @@
+puts "This is study drill 3 and calculates what is 10 - 5:"
+puts 10 - 5
---
+++ ex3.rb
@@ -0,0 +1,27 @@
+# Prints the string
+puts "I will now count my chickens:"
+# Prints the string, running the code after #{} first
+puts "Hens #{25.0 + 30.0 / 6.0}"
+# prints the string
+puts "Roosters #{100.0 - 25.0 * 3.0 % 4.0}"
+# prints the string
+puts "Now I will count the eggs:"
+# prints the result of the sum as a string
+puts 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
+# prints the string
+puts "Is it true that 3 + 2 < 5 - 7?"
+# prints the result of the sum as string
+puts  3.0 + 2.0 < 5.0 - 7.0
+
+# prints the string, running the sum first
+puts "What is 3 + 2? #{3.0 + 2.0}"
+# printst he string, running the sum first
+puts "What is 5 - 7 #{5.0 - 7.0}"
+# prints the string
+puts "Oh, so that's why it's false."
+#prints the string
+puts "How about some more."
+# prints the strings below, executing the ruby code first
+puts "Is it greater? #{5.0 > -2.0}"
+puts "IS it greater or equal? #{5.0 >= -2.0}"
+puts "Is it less or equal? #{5.0 <= -2.0}"
